d02137cef229b0b04cbb7cf6f3ce5b32d487673f
siripravallika00 <siripravallika00@gmail.com>
2026-09-04 15:34:19 +0530

    Add files via upload
    this is logical operators

---
+++ logical opertors.py
@@ -0,0 +1,21 @@
+# python logical operators
+#and(false)
+a=2
+result= a > 5 and  a<2 # logical and operator  
+print("Result of ",a,"> 5 and",a,"<2 is",result)
+
+#or(true)
+a=6
+result= a >5 or  a<2 # logical or operator
+print("Result of ",a,"> 5 or",a,"<2 is",result)
+
+#not(true)
+a=7 
+result= not(a >5 and  a<2) # logical not operator
+print("Result of not(",a,"> 5 and",a,"<2) is",result)
+
+
+
+
+
+
