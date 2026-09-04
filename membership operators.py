73ee9fba89ffa4ed572d69d18dd9c5500cace58e
siripravallika00 <siripravallika00@gmail.com>
2026-09-04 14:27:24 +0530

    Add files via upload
    this is membership operators

---
+++ membership operators.py
@@ -0,0 +1,22 @@
+#membership operators exercises
+#in
+a=[1,2,3]
+b=2
+result= b in a # membership operator  
+print("Result of ",b,"in",a,"is",result)
+
+a= [1,2,3]
+b=4  
+result= b in a # membership operator
+print("Result of ",b,"in",a,"is :",result)
+
+#not in
+a= [1,2,3]
+b=4
+result= b not in a # membership operator
+print("Result of ",b,"not in",a,"is :",result)
+
+a= [1,2,3]
+b=2
+result= b not in a # membership operator
+print("Result of ",b,"not in",a,"is :",result)
