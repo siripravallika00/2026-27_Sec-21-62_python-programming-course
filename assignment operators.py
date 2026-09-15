99df376ece41e5fdf9d89588abccaa2e793e2884
siripravallika00 <siripravallika00@gmail.com>
2026-09-15 13:23:29 +0530

    Add files via upload

---
+++ assignment operators.py
@@ -0,0 +1,16 @@
+#assignment operators exercises
+#augmented assignment operators
+x=5
+print("x =",x)
+x+=3 # equivalent to x=x+3
+print("x += 3 => x =",x)
+x-=2 # equivalent to x=x-2
+print("x -= 2 => x =",x)
+x*=4 # equivalent to x=x*4
+print("x *= 4 => x =",x)
+x//=3 # equivalent to x=x//3
+print("x //= 3 => x =",x)
+x%=2 # equivalent to x=x%2
+print("x %= 2 => x =",x)
+x**=2 # equivalent to x=x**2
+print("x **= 2 => x =",x)
