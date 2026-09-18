2299c553ac488f407c2023b37c157cb8cfdb0ca1
siripravallika00 <siripravallika00@gmail.com>
2026-09-18 14:35:20 +0530

    Add files via upload

---
+++ converting temperature.py
@@ -0,0 +1,4 @@
+# convert temperature from celsius to fahrenheit(4)
+celsius =int(input("Enter temperature in Celsius:"))
+fahrenheit=(celsius*9/5)+32
+print("Temperature in Fahrenheit:",fahrenheit)
