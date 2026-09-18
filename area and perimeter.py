fe62f66c6f7e29eb7974faff77f25bf8ace7bed9
siripravallika00 <siripravallika00@gmail.com>
2026-09-18 14:34:52 +0530

    Add files via upload

---
+++ area and perimeter.py
@@ -0,0 +1,7 @@
+# area and perimeter of a reactangle(3)
+num1=int(input("Enter the num1:"))
+num2=int(input("Enter te num2:"))
+area=num1*num2
+perimeter=2*(num1+num2)
+print("Area of rectangle",area)
+print("perimeter of rectangle",perimeter)
