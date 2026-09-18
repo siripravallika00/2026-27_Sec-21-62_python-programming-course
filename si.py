5135e6823be0d6a7ced05d866ef345b5e45f3103
siripravallika00 <siripravallika00@gmail.com>
2026-09-18 14:35:56 +0530

    Add files via upload

---
+++ si.py
@@ -0,0 +1,6 @@
+#to calucuate a simple intrest(6)
+p=int(input("Enter principal amount:")) 
+r=int(input("Enter rate of interest:")) 
+t=int(input("Enter time in years:"))
+si=(p*r*t)/100
+print("Simple Interest is:",si)
