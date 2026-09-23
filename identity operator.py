ee1f094b6b534deadc343ce9cd48bbceb8c4e2ac
siripravallika00 <siripravallika00@gmail.com>
2026-09-23 19:06:48 +0530

    Add files via upload

---
+++ identity operator.py
@@ -0,0 +1,8 @@
+#identity operatoes(12)
+a=[1,2,3]
+b=a 
+result=b is a #is membership operator
+print("result of ",b, "is not",a,"is :",result)
+a=[1,2,3]
+result=b is a # is membership operator
+print("result of",b, "is not ",a,"is :",result)  
