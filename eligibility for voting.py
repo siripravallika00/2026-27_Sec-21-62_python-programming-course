e670c7ad36b9cdf58deb00471d08e4956bc8f435
siripravallika00 <siripravallika00@gmail.com>
2026-09-25 16:09:43 +0530

    Add files via upload

--- eligibility for voting.py
+++ eligibility for voting.py
@@ -1,6 +1,7 @@
 #to check eligibility for voting using if-else
-age_in_years =int(input(enter the number:))
+age_in_years =int(input("Enter the number: "))
 if age_in_years >= 18:
     print(f"Age {age_in_years} years: Eligible to vote")
 else:
     print(f"Age {age_in_years} years: Not eligible to vote")
+    
